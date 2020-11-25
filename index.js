57bb49cb9c97d45a7027f61c36b38a0df1b2ebbb
Jules Kulcsar <kulcsar_jules@yahoo.com>
2020-11-25 22:50:29 +0200

    added 2nd solution visualization

--- index.js
+++ index.js
@@ -1,171 +1,293 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 let argArr = [];
 let argArrString = [];
 let jsx;
+let jsx2;
 let scriptTag;
+let scriptTag2;
 let testing;
+let testing2;
 let timeElapsed;
+let timeElapsed2;
 let arr = [];
+let arr2 = [];
 let inputArgs = [];
 let chartLabel = 0;
 let chartLabelArr = [];
-let submit = document.getElementById("submit");
-let errorMsg = document.getElementById("errorMsg");
+let setDisplay = false;
+let editor2;
+let submit = document.querySelector('.submit');
+let errorMsg = document.querySelector('.errorMsg');
+let errorMsg2 = document.querySelector('.errorMsg2');
+let codearea2 = document.getElementById('codearea2');
+let addSolutionButton = document.querySelector('.add_2nd_solution');
 
 //-----------generate chart----------//
 function chart() {
-  var ctx = document.getElementById("myChart");
-  var myChart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: chartLabelArr,
-      datasets: [
-        {
-          label: "Time Elapsed",
-          data: arr,
-          borderColor: "rgb(223, 117, 20)",
-          fill: false
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: chartLabelArr,
+            datasets: [
+                {
+                    label: 'Time Elapsed Solution1',
+                    data: arr,
+                    borderColor: 'rgb(223, 117, 20)',
+                    fill: false
+                }
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }
+                ],
+                xAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }
+                ]
+            },
+            tooltips: {
+                mode: 'point'
+            },
+            title: {
+                display: true,
+                text: 'Performance Visualisation',
+                fontSize: 20,
+                fontColor: '#CBDAE5'
+            },
+            events: ['click']
         }
-      ]
-    },
-    options: {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true
-            }
-          }
-        ]
-      },
-      tooltips: {
-        mode: "point"
-      },
-      title: {
-        display: true,
-        text: "Performance Visualisation",
-        fontSize: 20,
-        fontColor: "#CBDAE5"
-      },
-      events: ["click"]
-    }
-  });
+    });
 }
-//-----------generate chart----------//
+
+function chartTwoSolution() {
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: chartLabelArr,
+            datasets: [
+                {
+                    label: 'Time Elapsed Solution1',
+                    data: arr,
+                    borderColor: 'rgb(223, 117, 20)',
+                    fill: false
+                },
+                {
+                    label: 'Time Elapsed Solution2',
+                    data: arr2,
+                    borderColor: 'green',
+                    fill: false
+                }
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }
+                ],
+                xAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }
+                ]
+            },
+            tooltips: {
+                mode: 'point'
+            },
+            title: {
+                display: true,
+                text: 'Performance Visualisation',
+                fontSize: 20,
+                fontColor: '#CBDAE5'
+            },
+            events: ['click']
+        }
+    });
+}
 
 //---------codearea editor----------//
-const editor = CodeMirror.fromTextArea(document.getElementById("codearea"), {
-  lineNumbers: true,
-  mode: "javascript",
-  theme: "lucario",
-  lineWrapping: true,
-  matchBrackets: true,
-  autoCloseTags: true
+const editor = CodeMirror.fromTextArea(document.getElementById('codearea'), {
+    lineNumbers: true,
+    mode: 'javascript',
+    theme: 'lucario',
+    lineWrapping: true,
+    matchBrackets: true,
+    autoCloseTags: true
 });
-//---------codearea editor----------//
+
+//---------codearea2 editor----------//
+let changeDisplay = () => {
+    if (setDisplay === false) {
+        codearea2.style.display = 'block';
+        errorMsg2.style.display = 'block';
+        setDisplay = true;
+        editor2 = CodeMirror.fromTextArea(
+            document.getElementById('codearea2'),
+            {
+                lineNumbers: true,
+                mode: 'javascript',
+                theme: 'lucario',
+                lineWrapping: true,
+                matchBrackets: true,
+                autoCloseTags: true
+            }
+        );
+        chartTwoSolution();
+        addSolutionButton.innerHTML = 'Remove 2nd solution';
+        submit.innerHTML = 'call solutions';
+    } else {
+        editor2.toTextArea();
+        codearea2.style.display = 'none';
+        errorMsg2.style.display = 'none';
+        setDisplay = false;
+        chart();
+        addSolutionButton.innerHTML = 'Add 2nd solution';
+        submit.innerHTML = 'call solution';
+    }
+};
+addSolutionButton.addEventListener('click', changeDisplay);
 
 //--------------click handler on RUN--------------------//
 let clickHandler = () => {
-  // e.preventDefault();
-  getFirstInput();
-  getSecondInput();
-  chartTest(argArr);
+    // e.preventDefault();
+    getFirstInput();
+    getSecondInput();
+    chartTest(argArr);
 
-  chartLabel = 0;
+    chartLabel = 0;
 
-  document.createElement("script").remove;
-  jsx = editor.getValue();
-  scriptTag = document.createElement("script");
-  scriptTag.setAttribute("id", "toEval");
-  scriptTag.textContent = jsx;
+    document.createElement('script').remove;
 
-  try {
-    //----------create function from user input in codearea------------//
-    let body = scriptTag.textContent;
-    let wrap = s => "{ return " + body + " };"; //return the block having function expression
-    let func = new Function(wrap(body));
-    //----------create function tfrom user input in codearea------------//
+    jsx = editor.getValue();
+    scriptTag = document.createElement('script');
+    scriptTag.setAttribute('id', 'toEval');
+    scriptTag.textContent = jsx;
 
-    testing = func.apply(null, argArrString);
-    //--------calculate time elapsed-------------
-    let t1 = performance.now();
-    let t = testing(inputArgs[0], inputArgs[1]);
-    let t2 = performance.now();
-    timeElapsed = t2 - t1;
-    arr.push(timeElapsed);
-    //--------calculate time elapsed-------------
+    jsx2 = editor2.getValue();
+    scriptTag2 = document.createElement('script');
+    scriptTag2.setAttribute('id', 'toEval2');
+    scriptTag2.textContent = jsx2;
 
-    argArrString.length = 0;
-    inputArgs.length = 0;
-    errorMsg.value = `Output: ${t}`;
+    try {
+        //----------create function from user input in codearea------------//
+        let body = scriptTag.textContent;
+        if (body.length > 0) {
+            let wrap = s => '{ return ' + body + ' };'; //return the block having function expression
+            let func = new Function(wrap(body));
+            testing = func.apply(null, argArrString);
+            //--------calculate time elapsed-------------
+            let t1 = performance.now();
+            let t = testing(inputArgs[0], inputArgs[1]);
+            let t2 = performance.now();
+            timeElapsed = t2 - t1;
+            arr.push(timeElapsed);
+            errorMsg.value = `Output: ${t}`;
+        }
 
-    //------draw chart---------//
-    chart();
-    //------draw chart---------//
-  } catch (err) {
-    errorMsg.value = "OOPSY DAISY Error: " + err.message;
-    console.log(errorMsg);
-  }
+        //----------create function from user input in codearea------------//
+        let body2 = scriptTag2.textContent;
+        if (body2.length > 0) {
+            let wrap2 = s => '{ return ' + body2 + ' };'; //return the block having function expression
+            let func2 = new Function(wrap2(body2));
+            testing2 = func2.apply(null, argArrString);
+            //--------calculate time elapsed-------------
+            let _t1 = performance.now();
+            let _t = testing2(inputArgs[0], inputArgs[1]);
+            let _t2 = performance.now();
+            timeElapsed2 = _t2 - _t1;
+            arr2.push(timeElapsed2);
+
+            errorMsg2.value = `Output: ${_t}`;
+        }
+
+        //---------------reset arg array length----------------------------//
+        argArrString.length = 0;
+        inputArgs.length = 0;
+
+        //----------------------draw chart-------------------------------//
+        setDisplay ? chartTwoSolution() : chart();
+    } catch (err) {
+        errorMsg.value = 'OOPSY DAISY Error: ' + err.message;
+        console.log(errorMsg);
+    }
 };
-//--------------click handler on RUN--------------------//
 
 //-----------get func args from input field-------------//
 function getFirstInput() {
-  let inputVal = document.getElementById("arg1").value;
-  if (inputVal !== "" && Array.isArray(JSON.parse(inputVal)) === true) {
-    argArrString.push(inputVal);
-    let arrLength = calcArrLength(JSON.parse(inputVal));
-    argArr.push(arrLength);
-    inputArgs.push(JSON.parse(inputVal));
-  } else if (inputVal !== "" && typeof parseInt(inputVal) === "number") {
-    argArrString.push(inputVal);
-    argArr.push(parseInt(inputVal));
-    inputArgs.push(parseInt(inputVal));
-  }
-  return argArr;
+    let inputVal = document.getElementById('arg1').value;
+    if (inputVal !== '' && Array.isArray(JSON.parse(inputVal)) === true) {
+        argArrString.push(inputVal);
+        let arrLength = calcArrLength(JSON.parse(inputVal));
+        argArr.push(arrLength);
+        inputArgs.push(JSON.parse(inputVal));
+    } else if (inputVal !== '' && typeof parseInt(inputVal) === 'number') {
+        argArrString.push(inputVal);
+        argArr.push(parseInt(inputVal));
+        inputArgs.push(parseInt(inputVal));
+    }
+    return argArr;
 }
 
 function getSecondInput() {
-  let inputVal = document.getElementById("arg2").value;
-  if (inputVal !== "" && Array.isArray(JSON.parse(inputVal)) === true) {
-    argArrString.push(inputVal);
-    let arrLength = calcArrLength(JSON.parse(inputVal));
-    argArr.push(arrLength);
-    inputArgs.push(JSON.parse(inputVal));
-  } else if (inputVal !== "" && typeof parseInt(inputVal) === "number") {
-    argArrString.push(inputVal);
-    argArr.push(parseInt(inputVal));
-    inputArgs.push(parseInt(inputVal));
-  }
-  return argArr;
+    let inputVal = document.getElementById('arg2').value;
+    if (inputVal !== '' && Array.isArray(JSON.parse(inputVal)) === true) {
+        argArrString.push(inputVal);
+        let arrLength = calcArrLength(JSON.parse(inputVal));
+        argArr.push(arrLength);
+        inputArgs.push(JSON.parse(inputVal));
+    } else if (inputVal !== '' && typeof parseInt(inputVal) === 'number') {
+        argArrString.push(inputVal);
+        argArr.push(parseInt(inputVal));
+        inputArgs.push(parseInt(inputVal));
+    }
+    return argArr;
 }
-//-----------get func args from input field-------------//
 
 function chartTest(argArr) {
-  if (argArr.length === 1) {
-    chartLabel = argArr[0];
-    chartLabelArr.push(chartLabel);
-  } else {
-    for (let i = 0; i < argArr.length; i++) {
-      chartLabel = argArr[i];
+    if (argArr.length === 1) {
+        chartLabel = argArr[0];
+        chartLabelArr.push(chartLabel);
+    } else {
+        for (let i = 0; i < argArr.length; i++) {
+            chartLabel = argArr[i];
+        }
+        chartLabelArr.push(chartLabel);
     }
-    chartLabelArr.push(chartLabel);
-  }
 }
-
-chart();
 
 function calcArrLength(arr) {
-  let count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (typeof parseInt(arr[i]) === "number") {
-      count++;
-    } else if (Array.isArray(JSON.parse(arr[i]))) {
-      calcArrLength(JSON.parse(arr[i]));
-    } else {
-      alert("Please use only arrays and/or numbers as function arguments");
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof parseInt(arr[i]) === 'number') {
+            count++;
+        } else if (Array.isArray(JSON.parse(arr[i]))) {
+            calcArrLength(JSON.parse(arr[i]));
+        } else {
+            alert(
+                'Please use only arrays and/or numbers as function arguments'
+            );
+        }
     }
-  }
-  return count;
+    return count;
 }
 
-submit.addEventListener("click", clickHandler);
+setDisplay ? chartTwoSolution() : chart();
+
+submit.addEventListener('click', clickHandler);
